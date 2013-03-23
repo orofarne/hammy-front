4f7169ae9f5bad5eb98db51663f9ce55519f307b
Maxim Dementyev <max@orofarne.ru>
2013-03-24 01:54:01 +0400

    states stub

---
+++ routes/states.rb
@@ -0,0 +1,19 @@
+class HammyFront < Sinatra::Base
+	get '/states' do
+		haml :state_selector, :locals => {
+			:activetab => :view,
+			:title => 'states',
+		}
+	end
+
+	get '/states/show' do
+		hostname = params[:hostname]
+
+		haml :state, :locals => {
+			:activetab => :view,
+			:title => 'State',
+			:hostname => hostname,
+			:dataurl => '/data',
+		}
+	end
+end
