157c1fb6edd940bba886e351e713f4aa0bf3de4b
Maxim Dementyev <max@orofarne.ru>
2013-03-24 00:44:30 +0400

    generators

--- public/js/hammy.js
+++ public/js/hammy.js
@@ -13,3 +13,31 @@ var create_editor = function() {
 		$('#code').val(code_json);
 	});
 }
+
+var create_gen_editors = function() {
+	var mapeditor = ace.edit("mapeditor");
+	mapeditor.setTheme("ace/theme/xcode");
+	mapeditor.getSession().setMode("ace/mode/ruby");
+	var reduceditor = ace.edit("reduceditor");
+	reduceditor.setTheme("ace/theme/xcode");
+	reduceditor.getSession().setMode("ace/mode/ruby");
+
+	var code = JSON.parse($('#code').val());
+
+	mapeditor.setValue(code.mapcode);
+	mapeditor.getSession().getSelection().clearSelection();
+	reduceditor.setValue(code.reducecode);
+	reduceditor.getSession().getSelection().clearSelection();
+
+	var codeOnChange = function(e) {
+		var code = {
+			mapcode: mapeditor.getValue(),
+			reducecode: reduceditor.getValue()
+		};
+		var code_json = JSON.stringify(code);
+		$('#code').val(code_json);
+	}
+
+	mapeditor.getSession().on('change', codeOnChange);
+	reduceditor.getSession().on('change', codeOnChange);
+}
--- routes/generators.rb
+++ routes/generators.rb
@@ -11,4 +11,68 @@ class HammyFront < Sinatra::Base
 			:genlist => genlist,
 		}
 	end
+
+	get '/generators/create' do
+		haml :generators_edit, :locals => {
+			:activetab => :config,
+			:title => 'Create generator',
+			:generatorname => '',
+			:tag => '',
+			:regexp => '',
+			:code_json => JSON.dump({:mapcode => '', :reducecode => ''}),
+		}
+	end
+
+	get '/generators/edit' do
+		generatorname = params[:generatorname]
+		halt 400 if not generatorname or generatorname.empty?
+
+		g = Generator.find_by_name(generatorname) or halt 404
+		tagname = ''
+		tagname = g.tag.name if g.tag
+
+		haml :generators_edit, :locals => {
+			:activetab => :config,
+			:title => 'Create generator',
+			:generatorname => g.name,
+			:tag => tagname,
+			:regexp => g.regexp,
+			:code_json => JSON.dump({
+				:mapcode => g.mapcode,
+				:reducecode => g.reducecode,
+			}),
+		}
+	end
+
+	post '/generators/save' do
+		newgenerator = params[:newgenerator].to_i > 0
+		generatorname = params[:generatorname]
+		tagname = params[:tag]
+		regexp = params[:regexp]
+		code_json = params[:code]
+		code = JSON.parse(code_json)
+
+		ActiveRecord::Base.transaction do
+			t = Tag.find_by_name tagname
+			if newgenerator then
+				g = Generator.create :name => generatorname, :mapcode => code['mapcode'], :reducecode => code['reducecode']
+				g.tag = t if t
+				g.regexp = regexp unless !regexp or regexp.empty?
+				g.save
+			else
+				g = Generator.find_by_name(generatorname) or halt 404
+				g.tag = t
+				g.regexp = nil
+				g.regexp = regexp unless !regexp or regexp.empty?
+				g.mapcode = code['mapcode']
+				g.reducecode = code['reducecode']
+				g.save
+			end
+		end
+
+		haml :done, :locals => {
+			:activetab => nil,
+			:title => 'Done',
+		}
+	end
 end
