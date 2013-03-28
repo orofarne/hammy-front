7510fe60cb380ca8708eded8cf474e8786d29582
Maxim Dementyev <max@orofarne.ru>
2013-03-28 05:59:34 +0400

    validators fixed

--- models/models.rb
+++ models/models.rb
@@ -13,7 +13,7 @@ class Tag < ActiveRecord::Base
 	attr_accessible :name, :triggercode
 
 	validates_length_of :name, :minimum => 2, :maximum => 255
-	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
+	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\. ]*[0-9A-Za-z_]$/
 
 	has_many :hoststags
 	has_many :hosts, :through => :hoststags
@@ -29,7 +29,7 @@ class Generator < ActiveRecord::Base
 	attr_accessible :name, :regexp, :mapcode, :reducecode
 
 	validates_length_of :name, :minimum => 2, :maximum => 255
-	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
+	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\. ]*[0-9A-Za-z_]$/
 	validates_length_of :regexp, :minimum => 2, :maximum => 255
 
 	belongs_to :tag
