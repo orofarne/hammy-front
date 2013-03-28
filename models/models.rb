e9eae26a63a73a461f22a79b5e6cd1a917b4ddef
Maxim Dementyev <max@orofarne.ru>
2013-03-28 05:21:47 +0400

    minimum hostname length to 2

--- models/models.rb
+++ models/models.rb
@@ -1,7 +1,7 @@
 class Host < ActiveRecord::Base
 	attr_accessible :name, :triggercode
 
-	validates_length_of :name, :minimum => 3, :maximum => 255
+	validates_length_of :name, :minimum => 2, :maximum => 255
 	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
 
 	has_many :hoststags
@@ -12,7 +12,7 @@ end
 class Tag < ActiveRecord::Base
 	attr_accessible :name, :triggercode
 
-	validates_length_of :name, :minimum => 3, :maximum => 255
+	validates_length_of :name, :minimum => 2, :maximum => 255
 	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
 
 	has_many :hoststags
@@ -28,7 +28,7 @@ end
 class Generator < ActiveRecord::Base
 	attr_accessible :name, :regexp, :mapcode, :reducecode
 
-	validates_length_of :name, :minimum => 3, :maximum => 255
+	validates_length_of :name, :minimum => 2, :maximum => 255
 	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
 	validates_length_of :regexp, :minimum => 2, :maximum => 255
 
@@ -39,6 +39,6 @@ end
 class Trigger < ActiveRecord::Base
 	attr_accessible :host, :trigger
 
-	validates_length_of :host, :minimum => 3, :maximum => 255
+	validates_length_of :host, :minimum => 2, :maximum => 255
 	validates_format_of :host, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\.]*[0-9A-Za-z_]$/
 end
