73fd4624d930c4254aa75bce86b8c48a562f1cdb
Maxim Dementyev <max@orofarne.ru>
2013-03-28 06:33:47 +0400

    validators fixed

--- models/models.rb
+++ models/models.rb
@@ -29,7 +29,6 @@ class Generator < ActiveRecord::Base
 	attr_accessible :name, :regexp, :mapcode, :reducecode
 
 	validates_length_of :name, :minimum => 2, :maximum => 255
-	validates_format_of :name, :with => /^[0-9A-Za-z_][0-9A-Za-z\-_\. ]*[0-9A-Za-z_]$/
 	validates_length_of :regexp, :minimum => 2, :maximum => 255
 
 	belongs_to :tag
